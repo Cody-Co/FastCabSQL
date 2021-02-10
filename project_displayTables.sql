1cd4eef9365e938c02a1963ccc0e56b2b004d34c
Cody Covington <c.covington1995@gmail.com>
2021-02-09 21:05:47 -0800

    CSE 572 FastCabs SQL Database Project

---
+++ project_displayTables.sql
@@ -0,0 +1,13 @@
+-- CSE 572 Spring 2020
+-- Project Option: The FastCabs
+-- Purpose: Display table data
+-- Author: Cody-Co  
+
+SELECT * FROM ppas.office;
+SELECT * FROM ppas.staff;
+SELECT * FROM ppas.taxi;
+SELECT * FROM ppas.assignment;
+SELECT * FROM ppas.private;
+SELECT * FROM ppas.business;
+SELECT * FROM ppas.contract;
+SELECT * FROM ppas.job;
